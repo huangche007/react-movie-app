dcdd89c63d1f8d50f387d8e2e882771ff8a91173
huangche007 <hc386271623@163.com>
2019-07-28 22:53:40 +0800

    test cors

--- src/component/movie/MovieContainer.jsx
+++ src/component/movie/MovieContainer.jsx
@@ -1,10 +1,53 @@
 import React, { Component } from 'react';
+import {Route,Link} from 'react-router-dom'
+import { Layout, Menu } from 'antd';
+import MovieList from './MovieList'
+const { SubMenu } = Menu;
+const { Header, Content, Sider } = Layout;
 class MovieContainer extends Component {
     render() {
         return (
-            <div>
-                MovieContainer
-            </div>
+            <Layout style={{height:'100%'}}>
+            <Sider width={200} style={{ background: '#fff' }}>
+              <Menu
+                mode="inline"
+                defaultSelectedKeys={['1']}
+                style={{ height: '100%', borderRight: 0 }}
+              >
+                  <Menu.Item key="1">
+                      <Link to="/movie/in_threaters/1">
+                        正在热映
+                      </Link>
+                  </Menu.Item>
+                  <Menu.Item key="2">
+                      <Link to="/movie/comming_soon/1">
+                        即将上映
+                      </Link>
+                  </Menu.Item>
+                  <Menu.Item key="3">
+                     <Link to="/movie/top250/1">
+                        Top250
+                      </Link>
+                  </Menu.Item>
+            
+              </Menu>
+            </Sider>
+            <Layout style={{paddingLeft:'1px' }}>
+             
+              <Content
+                style={{
+                  background: '#fff',
+                  padding: 10,
+                  margin: 0,
+                  minHeight: 280,
+                }}
+              >
+                <Route path="/movie/:type/:page" component={MovieList}>
+
+                </Route>
+              </Content>
+            </Layout>
+          </Layout>
         );
     }
 }
--- src/component/movie/MovieList.jsx
+++ src/component/movie/MovieList.jsx
@@ -1,12 +1,55 @@
 import React, { Component } from 'react';
-
+import { Spin, Alert } from 'antd';
+import fetchJsonp from 'fetch-jsonp'
 class MovieList extends Component {
+
+    constructor(props){
+        super(props);
+        this.state = {
+            movies:[],
+            nowPage:props.match.params.page,
+            pageSize:14,
+            total:0,
+            isLoading:true
+        }
+    }
+    componentWillMount(){
+        // fetch('https://www.baidu.com').then((response)=>{
+        //     return response.json()//es6中的fecth api第一个then返回的response是一个对象
+        // }).then((mData)=>{
+        //     console.log('Mdata:',mData)
+        // })
+
+        this.loadMoiveListByTypeAndPage()
+    }
+
+    loadMoiveListByTypeAndPage(){
+        fetch('/api/movie/in_theaters')
+            .then(res => res.json())
+            .then(data=>{
+                console.log(data)
+            })
+    }
     render() {
         return (
             <div>
-                电影列表
+               {this.renderMovie()}
             </div>
         );
+    }
+
+    renderMovie(){
+        if(this.state.isLoading){
+            return <Spin tip="Loading...">
+            <Alert
+            message="数据请求中..."
+            description="电影列表马上呈现"
+            type="info"
+            />
+            </Spin>
+        }else{
+            return <h1>哈哈哈</h1>
+        }
     }
 }
 
